cead3efa1d461b3f382c07fb92dad768714e0270
DKaruhn <dkaruhn@proton.me>
2025-07-02 21:04:35 +0200

    add google bewertung

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -54,12 +54,47 @@ export class HomeComponent {
     { label: 'Sonstiges', value: 'Sonstiges' }
   ];
 
+
+  googleReviews = [
+    {
+      name: 'Wo Wu',
+      text: `Nachdem mich die "Fachfirma", bei der ich seit mehreren Jahren Kunde war (inklusive Wartungsvertrag) nach mehreren Wochen (!) des Herumeierns von jetzt auf gleich schmählich im Stich gelassen hatte, sorgte Herr Kortschak innerhalb kürzester Zeit dafür, dass meine Viessmann-Heizung (eine harte Nuss!) wieder lief. Herr Kortschak ist buchstäblich ein Meister seines Fachs, zuverlässig und geradezu unglaublich hartnäckig auch und gerade bei schwereren Fällen wie diesem. Ich kann mich der Einschätzung meines "Leidensgenossens" Herrn Schuberth nur anschließen: Herr Kortschak ist die ERSTE WAHL. Leider kann ich nur fünf Sterne vergeben. Deshalb ist hier noch einer: *`
+    },
+    {
+      name: 'Horst Schuberth',
+      text: `Es ging um eine Öl-Brennwert-Heizung. Keine 2 Jahre alt. Die Heizung hatte keine Lust mehr zu funktionieren. Eine Fehlersuche ergab, dass zuerst die Ölpumpe und nach dieser Reparatur auch noch der Zündtrafo defekt waren. Dann lief endlich die Anlage. Mehrere Tests (An/Aus) wurden durchgeführt. Mein Urteil: Vertrauenswürdig, kompetent, schnell, engagiert und zuverlässig. Herr Kortschak ist die ERSTE WAHL`
+    },
+    {
+      name: 'Sylvia Kipry',
+      text: `Absoluter zuverlässig, leistet top Arbeit. Auf jeden Fall weiter zuempfehlen !!!!`
+    },
+    {
+      name: 'Markus Unger',
+      text: `Hatten bei unser doch relativ alten Vailant Heizung (von 1999) eine Störung und mehrere Probleme, so dass die Heizung sich nach wenigen Minuten von selbst ausgeschaltet hat. Der Herr Kortschak war nach einem kurzen Telefongespräch am nächsten Tag vor Ort und konnte alles beheben. Super freundlich und großartige Arbeit. Eine klare Empfehung!`
+    }
+  ];
+
+
   constructor(private messageService: MessageService) {
     setInterval(() => {
       this.currentImageIndex =
         (this.currentImageIndex + 1) % this.heroImages.length;
     }, 8000);
   }
+
+  selectedReview: any = null;
+  reviewDialogVisible = false;
+
+  openReviewDialog(review: any) {
+    this.selectedReview = review;
+    this.reviewDialogVisible = true;
+  }
+
+  closeReviewDialog() {
+    this.reviewDialogVisible = false;
+    this.selectedReview = null;
+  }
+
 
   confirmAppointment() {
     if (this.appointmentName && this.appointmentDate && this.address) {
